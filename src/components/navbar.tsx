05f3f43f5eae1a0b631bd94c3357c4ee4bd19076
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2023-12-28 02:25:10 -0800

    Banner UI + Font Imports + Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,56 @@
-//this is a static navbar
 import logo from "../assets/images/msub-logo.png";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="navbar">
       <a href="#home" id="msub-logo">
         <img src={logo} alt="logo" className="logo" />
       </a>
-      <a href="#about"> About </a>
-      <a href="#services"> Services </a>
-      <a href="#teams"> Team </a>
-      <a href="#summer"> Summer Program </a>
-      <a href="#history"> History </a>
-      <a href="/resources"> Resources </a>
+
+      {windowWidth <= 755 ? (
+        <div className="mobile-menu">
+          <button className="hamburger" onClick={toggleMenu}>
+            ☰
+          </button>
+          {showMenu && (
+            <div className={`mobile-menu-options ${showMenu ? "show" : ""}`}>
+              <a href="#about">About</a>
+              <a href="#team">Team</a>
+              <a href="#summer">Program</a>
+              <a href="#history">History</a>
+              <a href="/resources">Resources</a>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="desktop-menu">
+          <a href="#about">About</a>
+          <a href="#team">Team</a>
+          <a href="#summer">Program</a>
+          <a href="#history">History</a>
+          <a href="/resources">Resources</a>
+        </div>
+      )}
     </div>
   );
 };
